81ddca1175db38585767f95bac6ca2b411d753ba
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-10-08 04:29:54 -0400

    iseven

--- hello.js
+++ hello.js
@@ -1,7 +1,14 @@
 //
-const sayHello  = function (name) {
+const sayHelloToConsole  = function (name) {
   console.log("Hello, " + name);
 }
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+sayHelloToConsole('John'); 
+/*const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');*/
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
